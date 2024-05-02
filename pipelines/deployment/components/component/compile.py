47811669a60b2aea7e1aece956fe1f283c97851c
Davide Lupis <davide_lupis@yahoo.it>
2024-05-02 10:57:42 +0200

    update formatting requirements

--- pipelines/deployment/components/component/compile.py
+++ pipelines/deployment/components/component/compile.py
@@ -14,7 +14,6 @@ REGION = os.environ.get("REGION")
 REPOSITORY = os.environ.get("BUCKET_NAME")  # Match the Bucket name on Artifact Registry
 
 PIPELINE_NAME = os.path.basename(os.path.dirname(__file__))  # Match the directory name
-
 COMPONENT_NAME = os.path.basename(__file__).split(".")[0]  # Match the file name
 BASE_IMAGE = (
     f"{REGION}-docker.pkg.dev/{PROJECT_ID}/{REPOSITORY}/{PIPELINE_NAME}-image:latest"

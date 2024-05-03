51ae0997c2cca245b15bfcd4a6197be227151b23
Davide Lupis <davide_lupis@yahoo.it>
2024-05-03 22:23:17 +0200

    BigQueryConf refactor

--- pipelines/deployment/setup.py
+++ pipelines/deployment/setup.py
@@ -17,6 +17,7 @@ setup(
         "joblib==1.4.0",
         "pyyaml==6.0.1",
         "python-dotenv==1.0.1",
+        "pyarrow==16.0.0",
         "jinja2==3.1.3",
         "mkdocs==1.6.0",
         "black==24.4.2",
